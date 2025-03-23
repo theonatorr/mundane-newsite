9eb069549f8c492cf90da176795c8bf98d8b0508
Theo Nash <theo@theos-MacBook-Air-3.local>
2025-03-23 14:46:17 -0700

    sfdf

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '', // leave this blank for Vercel (unlike GitHub Pages)
+  base: '',
   plugins: [react()],
 });
